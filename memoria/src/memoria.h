828a2e49c7ac9f9908a22a012482efedf032b043
LeoMorsella <leomorsella@frba.utn.edu.ar>
2023-04-03 20:47:40 +0000

    Se establece modulo memoria y estructura general de proyecto

---
+++ memoria/src/memoria.h
@@ -0,0 +1,20 @@
+/*
+ * memoria.h
+ *
+ *  Created on: Apr 3, 2023
+ *      Author: utnso
+ */
+
+#ifndef MEMORIA_H_
+#define MEMORIA_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <commons/log.h>
+#include "utils.h"
+
+void iterator(char* value);
+
+
+#endif /* MEMORIA_H_ */
---
+++ memoria/src/utils.h
@@ -0,0 +1,39 @@
+/*
+ * utils.h
+ *
+ *  Created on: Apr 3, 2023
+ *      Author: utnso
+ */
+
+#ifndef UTILS_H_
+#define UTILS_H_
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<unistd.h>
+#include<netdb.h>
+#include<commons/log.h>
+#include<commons/collections/list.h>
+#include<string.h>
+#include<assert.h>
+
+#define PUERTO "4444"
+
+typedef enum
+{
+	MENSAJE,
+	PAQUETE
+}op_code;
+
+extern t_log* logger;
+
+void* recibir_buffer(int*, int);
+
+int iniciar_servidor(void);
+int esperar_cliente(int);
+t_list* recibir_paquete(int);
+void recibir_mensaje(int);
+int recibir_operacion(int);
+
+#endif /* UTILS_H_ */
