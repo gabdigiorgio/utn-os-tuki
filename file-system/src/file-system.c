3f02e80d7d829329d743f0abfa8ac58132eb0819
gabdigiorgio <gdigiorgio@frba.utn.edu.ar>
2023-04-04 05:01:10 +0000

    Se agrega modulo de file-system

--- file-system/src/file-system.c
+++ file-system/src/file-system.c
@@ -1,17 +1,86 @@
-/*
- ============================================================================
- Name        : file-system.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
+#include "file-system.h"
 
-#include <stdio.h>
-#include <stdlib.h>
+int main(void)
+{
+	int conexion;
+	char* ip;
+	char* puerto;
 
-int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+	t_log* logger;
+	t_config* config;
+
+	logger = iniciar_logger();
+	config = iniciar_config();
+
+	ip = config_get_string_value(config,"IP_MEMORIA");
+	puerto = config_get_string_value(config,"PUERTO_MEMORIA");
+
+	log_info(logger, "IP de la memoria: %s", ip);
+	log_info(logger, "Puerto de la memoria: %s", puerto);
+
+	// Creamos una conexión hacia el servidor (memoria)
+
+	conexion = crear_conexion(ip, puerto);
+
+	paquete(conexion);
+
+	terminar_programa(conexion, logger, config);
+
+}
+
+t_log* iniciar_logger(void)
+{
+	t_log* nuevo_logger = log_create("./file-system.log","File system",true,LOG_LEVEL_INFO);
+
+	return nuevo_logger;
+}
+
+t_config* iniciar_config(void)
+{
+	t_config* nuevo_config = config_create("./file-system.config");
+
+	return nuevo_config;
+}
+
+void leer_consola(t_log* logger)
+{
+	char* leido;
+
+	while(1){
+		leido = readline("> ");
+
+		if(strcmp(leido,"")==0)break;
+
+		log_info(logger,leido);
+		free(leido);
+	}
+
+}
+
+void paquete(int conexion)
+{
+
+	char* leido;
+	t_paquete* paquete = crear_paquete();
+
+	while(1){
+		leido = readline("> ");
+
+		if(strcmp(leido,"")==0){
+			break;
+		}
+		agregar_a_paquete(paquete,leido,(strlen(leido)+1));
+		free(leido);
+	}
+
+	enviar_paquete(paquete,conexion);
+	eliminar_paquete(paquete);
+
+}
+
+void terminar_programa(int conexion, t_log* logger, t_config* config)
+{
+	log_destroy(logger);
+	config_destroy(config);
+	liberar_conexion(conexion);
 }
