ebe2f73b26542b8c4ef256a02f585b6cc7fd0222
Gabriel Rodriguez <gabrirodriguez@frba.utn.edu.ar>
2023-05-31 05:27:47 +0000

    preparando makefiles

---
+++ consola4/Debug/src/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/client_utils.c \
+../src/consola.c \
+../src/initial_setup.c \
+../src/utils.c 
+
+C_DEPS += \
+./src/client_utils.d \
+./src/consola.d \
+./src/initial_setup.d \
+./src/utils.d 
+
+OBJS += \
+./src/client_utils.o \
+./src/consola.o \
+./src/initial_setup.o \
+./src/utils.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c src/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ cpu/Debug/src/subdir.mk
@@ -0,0 +1,46 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/client_utils.c \
+../src/comm_thread.c \
+../src/cpu.c \
+../src/initial_setup.c \
+../src/server_utils.c \
+../src/utils.c 
+
+C_DEPS += \
+./src/client_utils.d \
+./src/comm_thread.d \
+./src/cpu.d \
+./src/initial_setup.d \
+./src/server_utils.d \
+./src/utils.d 
+
+OBJS += \
+./src/client_utils.o \
+./src/comm_thread.o \
+./src/cpu.o \
+./src/initial_setup.o \
+./src/server_utils.o \
+./src/utils.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c src/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
+clean: clean-src
+
+clean-src:
+	-$(RM) ./src/client_utils.d ./src/client_utils.o ./src/comm_thread.d ./src/comm_thread.o ./src/cpu.d ./src/cpu.o ./src/initial_setup.d ./src/initial_setup.o ./src/server_utils.d ./src/server_utils.o ./src/utils.d ./src/utils.o
+
+.PHONY: clean-src
+
