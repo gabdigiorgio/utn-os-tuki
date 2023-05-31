fe31a8988b38f11fea64f222678a211b5c84cbd9
Gabriel Rodriguez <gabrirodriguez@frba.utn.edu.ar>
2023-05-31 14:09:04 +0000

    add scripts

---
+++ scripts/start-all.sh
@@ -0,0 +1,4 @@
+../memoria/Debug/memoria ../config-files/memoria.config;
+../file-system/Debug/file-system ../config-files/file-system.config;
+../cpu/Debug/cpu ../config-files/cpu.config;
+../kernel/Debug/kernel ../config-files/kernel.config;
