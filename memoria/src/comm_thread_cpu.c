d1bfd0b76e8e9354a225e4309374601b8ef92088
gabdigiorgio <gdigiorgio@frba.utn.edu.ar>
2023-06-17 20:17:18 +0000

    Agrego MOV_OUT en memoria.

--- memoria/src/comm_thread_cpu.c
+++ memoria/src/comm_thread_cpu.c
@@ -19,21 +19,22 @@ void conexion_cpu(int server_connection)
 		case 1:
 			t_instruc_mem *nueva_instruccion = inicializar_instruc_mem();
 			deserializar_instruccion_memoria(nueva_instruccion, paquete->buffer, paquete->lineas);
-
+			int direccion_fisica;
 			switch (nueva_instruccion->estado)
 			{
 
 			case MOV_IN:
 				log_info(logger, "MOV_IN llego a memoria");
 				int valor;
-				memcpy(&valor, memoria + atoi(nueva_instruccion->param2), sizeof(int)); //fijarse la direccion (param2)
+				direccion_fisica = atoi(nueva_instruccion->param2);
+				memcpy(&valor, memoria + direccion_fisica, sizeof(int));
 				snprintf(nueva_instruccion->param2, sizeof(nueva_instruccion->param2), "%d", valor);
-				serializar_instruccion_memoria(server_connection, nueva_instruccion); //enviar valor a cpu
+				serializar_instruccion_memoria(server_connection, nueva_instruccion);
 				break;
 			case MOV_OUT:
 				log_info(logger, "MOV_OUT llego a memoria");
-				//int valor_registro = atoi(nueva_instruccion->param1););
-				//memcpy(memoria + atoi(nueva_instruccion->param1), &valor_registro, sizeof(int));
+				direccion_fisica = atoi(nueva_instruccion->param1);
+				memcpy(memoria + direccion_fisica, nueva_instruccion->param2, nueva_instruccion->param2_length);
 				break;
 			}
 
