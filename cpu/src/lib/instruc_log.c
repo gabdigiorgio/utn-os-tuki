a2a9fd25a6397cdd32fdc67290153804de4eb650
D-M-Gonzalez <dami.m.gonza@gmail.com>
2023-07-04 22:47:21 +0000

    fixes generales

--- cpu/src/lib/instruc_log.c
+++ cpu/src/lib/instruc_log.c
@@ -1,5 +1,5 @@
 #include "../../includes/code_reader.h"
 
 void log_instruccion (uint32_t pid, char* instruc, char* params){
-	log_info(logger, "PID: %d Ejecutando [%s] - [%s]",pid, instruc, params);
+	log_info(logger, "PID: %d - Ejecutando [%s] - [%s]",pid, instruc, params);
 }
